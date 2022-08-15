20f414e5164cb60086555d932dad701488904d75
Helen-Starc <109182577+Helen-Starc@users.noreply.github.com>
2022-08-16 00:37:28 +0200

    final version

--- js/modal.js
+++ js/modal.js
@@ -1,3 +1,4 @@
+//Добавляет и удаляет .is-hidden в .modal при открытии-закрытии
 (() => {
 	const refs = {
 		openModalBtn: document.querySelector("[data-modal-open]"),
@@ -10,5 +11,27 @@
 
 	function toggleModal() {
 		refs.modal.classList.toggle("is-hidden");
+	}
+})();
+
+// Добавляет .modal-animation в .modal при клике на
+//.hero-btn и удаляет при окончании анимации
+(() => {
+	const refs = {
+		modal: document.querySelector(".modal"),
+		herobtn: document.querySelector(".hero-btn"),
+	};
+
+	refs.herobtn.addEventListener("click", toggleModal);
+
+	function toggleModal() {
+		refs.modal.classList.toggle("modal-animation");
+	}
+	// Отслеживаем окончание анимации
+	refs.modal.addEventListener("animationend", AnimationHandler, false);
+
+	function AnimationHandler() {
+		// Удаляем класс с анимацией
+		refs.modal.classList.remove("modal-animation");
 	}
 })();
