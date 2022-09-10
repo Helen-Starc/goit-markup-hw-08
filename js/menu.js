d2c781a3949a3ed71e2a3aebada4e3ad8fdb231e
Helen-Starc <109182577+Helen-Starc@users.noreply.github.com>
2022-09-10 04:02:34 +0200

    final version

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,17 @@
 (() => {
 	const menuBtnRef = document.querySelector("[data-menu-button]");
 	const mobileMenuRef = document.querySelector("[data-menu]");
+	const menuBackdropRef = document.querySelector("[data-menu-backdrop]");
 
 	menuBtnRef.addEventListener("click", () => {
+		document.body.classList.toggle("menu-open");
+
 		const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false;
 
 		menuBtnRef.classList.toggle("is-open");
 		menuBtnRef.setAttribute("aria-expanded", !expanded);
 
+		menuBackdropRef.classList.toggle("is-open");
 		mobileMenuRef.classList.toggle("is-open");
 	});
 })();
--- js/modal.js
+++ js/modal.js
@@ -11,9 +11,32 @@
 	refs.openModalBtn.addEventListener("click", toggleModal);
 	refs.closeModalBtn.addEventListener("click", toggleModal);
 
+	function windowSize() {
+		windowHeight = window.innerHeight ? window.innerHeight : $(window).height();
+		windowWidth = window.innerWidth ? window.innerWidth : $(window).width();
+	}
+
 	function toggleModal() {
 		document.body.classList.toggle("modal-open");
 		refs.backdrop.classList.toggle("is-hidden");
-		refs.modal.classList.toggle("modal-animation");
+
+		// window.addEventListener("resize", function () {
+		// 	if (window.matchMedia("(width >= 768px)").matches) {
+		// 		console.log("Screen width is at least 768px");
+		// 		refs.modal.classList.toggle("modal-animation");
+		// 	} else {
+		// 		console.log("Screen less than 768px");
+		// 		refs.modal.classList.toggle("modal-mobile-animation");
+		// 	}
+		// });
+
+		windowSize();
+		console.log("width is :", windowWidth, "Height is :", windowHeight);
+		if (windowWidth < 480) {
+			console.log("width is under 480px !");
+			refs.modal.classList.toggle("modal-mobile-animation");
+		} else {
+			refs.modal.classList.toggle("modal-animation");
+		}
 	}
 })();
